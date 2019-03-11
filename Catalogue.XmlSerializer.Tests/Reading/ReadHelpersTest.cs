f4a35ddc752d629ae18ca0d465fd9fc3139267d0
avk <Andrew.Kostousov@gmail.com>
2019-03-11 13:12:56 +0500

    uncomment ReadHelpersTests

--- Catalogue.XmlSerializer.Tests/Reading/ReadHelpersTest.cs
+++ Catalogue.XmlSerializer.Tests/Reading/ReadHelpersTest.cs
@@ -1,92 +1,61 @@
-﻿//using System;
+using Catalogue.XmlSerializer.Reading;
+using Catalogue.XmlSerializer.Reading.ContentReaders;
 
-//using NUnit.Framework;
+using NUnit.Framework;
 
-//namespace SKBKontur.Catalogue.XmlSerializer.Tests.Reading
-//{
-//    [TestFixture]
-//    public class ReadHelpersTest
-//    {
-//        #region Setup/Teardown
+namespace Catalogue.XmlSerializer.Tests.Reading
+{
+    [TestFixture]
+    public class ReadHelpersTest
+    {
+        [Test]
+        public void TestHasPublicConstructor()
+        {
+            var emitConstruction = ReadHelpers.EmitConstruction<CWithPublic>();
+            Assert.AreEqual(0, CWithPublic.count);
+            var cWithPublic = emitConstruction();
+            Assert.AreEqual(1, cWithPublic.localCount);
+            Assert.AreEqual(1, CWithPublic.count);
+        }
 
-//        [SetUp]
-//        public void SetUp()
-//        {
-//            contentWriterCollection = GetMock<IContentReaderCollection>();
-//            contentReader = GetMock<IContentReader<int>>();
-//        }
+        [Test]
+        public void TestNoPublicConstructor()
+        {
+            var emitConstruction = ReadHelpers.EmitConstruction<CNoPublic>();
+            Assert.AreEqual(0, CNoPublic.count);
+            var result = emitConstruction();
+            Assert.AreEqual(0, result.localCount);
+            Assert.AreEqual(0, CNoPublic.count);
+        }
 
-//        #endregion
+        private IContentReaderCollection contentWriterCollection;
+        private IContentReader<int> contentReader;
 
-//        [Test]
-//        public void TestBuildSetter()
-//        {
-//            contentWriterCollection.Expect(collection => collection.Get<int>()).Return(contentReader);
+        public class CWithProp
+        {
+            public int A { get; set; }
+        }
 
-//            IContentPropertySetter<CWithProp> contentPropertySetter =
-//                ReadHelpers.BuildSetter<CWithProp>(typeof(CWithProp).GetProperty("A"),
-//                                                   contentWriterCollection);
+        private class CWithPublic
+        {
+            public CWithPublic()
+            {
+                localCount = ++count;
+            }
 
-//            var target = new CWithProp {A = 1};
+            public static int count;
+            public readonly int localCount;
+        }
 
-//            contentReader.Expect(reader => reader.Read(null)).Return(100);
-//            contentPropertySetter.SetProperty(target, null);
-//            Assert.AreEqual(100, target.A);
+        private class CNoPublic
+        {
+            private CNoPublic()
+            {
+                localCount = ++count;
+            }
 
-//            contentReader.Expect(reader => reader.Read(null)).Return(200);
-//            contentPropertySetter.SetProperty(target, null);
-//            Assert.AreEqual(200, target.A);
-//        }
-
-//        [Test]
-//        public void TestHasPublicConstructor()
-//        {
-//            Func<CWithPublic> emitConstruction = ReadHelpers.EmitConstruction<CWithPublic>();
-//            Assert.AreEqual(0, CWithPublic.count);
-//            CWithPublic cWithPublic = emitConstruction();
-//            Assert.AreEqual(1, cWithPublic.localCount);
-//            Assert.AreEqual(1, CWithPublic.count);
-//        }
-
-//        [Test]
-//        public void TestNoPublicConstructor()
-//        {
-//            Func<CNoPublic> emitConstruction = ReadHelpers.EmitConstruction<CNoPublic>();
-//            Assert.AreEqual(0, CNoPublic.count);
-//            CNoPublic result = emitConstruction();
-//            Assert.AreEqual(0, result.localCount);
-//            Assert.AreEqual(0, CNoPublic.count);
-//        }
-
-//        public class CWithProp
-//        {
-//            public int A { get; set; }
-//        }
-
-//        private IContentReaderCollection contentWriterCollection;
-//        private IContentReader<int> contentReader;
-
-//        private class CWithPublic
-//        {
-//            public CWithPublic()
-//            {
-//                localCount = ++count;
-//            }
-
-//            public static int count;
-//            public readonly int localCount;
-//        }
-
-//        private class CNoPublic
-//        {
-//            private CNoPublic()
-//            {
-//                localCount = ++count;
-//            }
-
-//            public static int count;
-//            public readonly int localCount;
-//        }
-//    }
-//}
-
+            public static int count;
+            public readonly int localCount;
+        }
+    }
+}
